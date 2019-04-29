3daef02c86fde07670195cccf35d687183dabc4f
Alex <alexcc17@gmail.com>
2019-04-29 16:34:07 -0400

    Commit

---
+++ Contained/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  Contained
+//
+//  Created by Alex on 4/29/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet var mySpritKitView: SKView!
+    
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        mySpritKitView.presentScene(skscene)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Contained/SettingsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsViewController.swift
+//  Contained
+//
+//  Created by Alex on 4/29/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        if sender.isOn == true {
+            sender.isOn = false
+        } else {
+            sender.isOn = true
+        }
+    }
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        if sender.isOn == true {
+            sender.isOn = false
+        } else {
+            sender.isOn = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
